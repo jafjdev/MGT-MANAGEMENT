4e1106a50422d9c60361decc49b8fdb87a273694
ramiroavch <31267624+ramiroavch@users.noreply.github.com>
2018-06-04 09:33:21 -0400

    ver proyectos

---
+++ script/Proy-Comp_Script.js
@@ -0,0 +1,59 @@
+var div = document.getElementById("proyectos");
+var dbref= firebase.database();
+var proyref = dbref.ref('Project');
+var id;
+var i;
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+   id=user.uid; 
+  } else {
+    // No user is signed in.
+  }
+});
+
+proyref.orderByValue() .on("value", function(data) {
+   	data.forEach(function(data) {
+   		data.forEach(function(data) {
+   				console.log(data.key);
+   				if(data.key=="uid_creador"){
+   					console.log(data.val());
+   					if(data.val()==id)
+   					{
+   						i=1;
+   					}
+   				}
+   		
+   		});
+   		if(i==1)
+   		{
+ 	  	div.innerHTML += proyectos(data.key);  	
+		}
+   	});
+ });
+
+function proyectos(val){
+var html = '';
+html +=' <div class="col-sm-6 col-lg-5">';
+	html +='<div class="card text-white bg-flat-color-1">';
+		html += '<div class="card-body pb-0">';
+			html += '<div class="dropdown float-right">';
+				html += '<button class="btn bg-transparent dropdown-toggle theme-toggle text-light"';
+				html += 'type="button" id="dropdownMenuButton" data-toggle="dropdown">';
+					html += '<i class="fa fa-cog"></i>';
+					html += '</button>';
+					html += '<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">';
+						html += '<div class="dropdown-menu-content">';
+							html += '<a class="dropdown-item" href="#">Action</a>';
+							html += '<a class="dropdown-item" href="#">Another action</a>';
+							html += '<a class="dropdown-item" href="#">Something else here</a>';
+						html += '</div>';
+					html += '</div>';
+			html += '</div>';
+			html += '<h4 class="mb-0">';
+				html += '<span>'+val+'</span>';
+			html += '</h4>';
+		html += '</div>';
+	html += '</div>';
+html += '</div>';
+return html;
+}
